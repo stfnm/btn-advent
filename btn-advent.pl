a4862466ef32e6ea32615384e304891c4863fb2a
stfn <stfnmd@gmail.com>
2012-12-18 19:39:06 +0100

    Update

--- btn-advent.pl
+++ btn-advent.pl
@@ -19,24 +19,14 @@
 
 use strict;
 use warnings;
-use File::Slurp;
 use WWW::Curl::Easy;
 use POSIX qw(strftime);
 
 # Time interval in seconds
 my $INTERVAL = 5 * 60;
 
-# Read cookies (for logged in BTN session) from files
-my $PHPSESSID = read_file('PHPSESSID.txt') or die;
-
-# Initialize curl stuff
-my $curl = WWW::Curl::Easy->new;
-my $response_body;
-
-$curl->setopt(CURLOPT_HEADER,1);
-$curl->setopt(CURLOPT_URL, 'https://broadcasthe.net/advent.php?action=claimprize');
-$curl->setopt(CURLOPT_COOKIE, "PHPSESSID=$PHPSESSID");
-$curl->setopt(CURLOPT_WRITEDATA, \$response_body);
+# Cookies file
+my $COOKIES = "cookies.txt";
 
 # Subroutine for logging
 sub verbose
@@ -49,19 +39,30 @@ print "Time interval is $INTERVAL seconds...\n";
 
 # Main infinite loop
 for (;;) {
-	$response_body = "";
-	my $retcode = $curl->perform();
+	# Curl scope
+	{
+		my $curl = WWW::Curl::Easy->new;
+		my $response_body;
 
-	if ($retcode == 0) {
-		if ($response_body =~ /<b>(\d+d \d+h \d+m \d+s)<\/b>/) {
-			verbose("Time left until next claim: $1\n");
-		} elsif ($response_body =~ /You have received the following prize:.*?<h1>(.*?)<\/h1>/) {
-			verbose("Yay, you got the following prize: $1\n");
+		$curl->setopt(CURLOPT_HEADER,1);
+		$curl->setopt(CURLOPT_URL, 'https://broadcasthe.net/advent.php?action=claimprize');
+		$curl->setopt(CURLOPT_COOKIEFILE, $COOKIES);
+		$curl->setopt(CURLOPT_COOKIEJAR, $COOKIES);
+		$curl->setopt(CURLOPT_WRITEDATA, \$response_body);
+
+		my $retcode = $curl->perform();
+
+		if ($retcode == 0) {
+			if ($response_body =~ /<b>(\d+d \d+h \d+m \d+s)<\/b>/) {
+				verbose("Time left until next claim: $1\n");
+			} elsif ($response_body =~ /You have received the following prize:.*?<h1>(.*?)<\/h1>/) {
+				verbose("Yay, you got the following prize: $1\n");
+			} else {
+				verbose("$response_body\n");
+			}
 		} else {
-			verbose("$response_body\n");
+			verbose("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
 		}
-	} else {
-		verbose("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
 	}
 
 	# Now we gotta sleep for a bit...
