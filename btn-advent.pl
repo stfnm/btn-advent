92a878ae97e4d16d400f6b15b064471af50177d7
stfn <stfnmd@gmail.com>
2013-12-05 16:49:43 +0100

    Add option "--login"

--- btn-advent.pl
+++ btn-advent.pl
@@ -20,6 +20,7 @@
 
 use strict;
 use warnings;
+use CGI;
 use Getopt::Long;
 use WWW::Curl::Easy;
 use POSIX qw(strftime);
@@ -28,13 +29,17 @@ use POSIX qw(strftime);
 my $OPT_INTERVAL = 5 * 60;
 my $OPT_COOKIES = "cookies.txt";
 my $OPT_QUIET = 0;
+my $OPT_LOGIN = "";
 
 GetOptions(
 	'help' => \&help,
+	'login=s' => \$OPT_LOGIN,
 	'cookies=s' => \$OPT_COOKIES,
 	'interval=i' => \$OPT_INTERVAL,
 	'quiet' => \$OPT_QUIET,
 );
+
+main();
 
 sub help
 {
@@ -43,6 +48,9 @@ Usage of $0:
 
 	-h, --help
 		Print help message and quit.
+
+	-l, --login=<username:password>
+		Perform login with the provided username and password.
 
 	-c, --cookies=<file name>
 		Cookies file in Netscape HTTP Cookie File format. (default: cookies.txt)
@@ -64,34 +72,60 @@ sub verbose
 	print @_;
 }
 
-# Main infinite loop
-for (;;) {
-	# Curl scope
-	{
-		my $curl = WWW::Curl::Easy->new;
-		my $response_body;
+sub btn_login($$)
+{
+	my ($user, $password) = @_;
+	my $curl = WWW::Curl::Easy->new;
+	my $response_body;
 
-		$curl->setopt(CURLOPT_HEADER, 1);
-		$curl->setopt(CURLOPT_URL, 'https://broadcasthe.net/advent.php?action=claimprize');
-		$curl->setopt(CURLOPT_COOKIEFILE, $OPT_COOKIES);
-		$curl->setopt(CURLOPT_COOKIEJAR, $OPT_COOKIES);
-		$curl->setopt(CURLOPT_WRITEDATA, \$response_body);
+	$curl->setopt(CURLOPT_HEADER, 1);
+	$curl->setopt(CURLOPT_URL, 'https://broadcasthe.net/login.php');
+	$curl->setopt(CURLOPT_COOKIEFILE, $OPT_COOKIES);
+	$curl->setopt(CURLOPT_COOKIEJAR, $OPT_COOKIES);
+	$curl->setopt(CURLOPT_POST, 1);
+	$curl->setopt(CURLOPT_POSTFIELDS, "username=" . CGI::escape($user) . "&password=" . CGI::escape($password));
+	$curl->setopt(CURLOPT_WRITEDATA, \$response_body);
 
-		my $retcode = $curl->perform();
+	$curl->perform();
+}
 
-		if ($retcode == 0) {
-			if ($response_body =~ /<b>(\d+d \d+h \d+m \d+s)<\/b>/) {
-				verbose("Time left until next claim: $1\n");
-			} elsif ($response_body =~ /You have received the following prize:.*?<h1>(.*?)<\/h1>/) {
-				verbose("Yay, you got the following prize: $1\n");
-			} else {
-				verbose("$response_body\n");
-			}
+sub btn_advent
+{
+	my $curl = WWW::Curl::Easy->new;
+	my $response_body;
+
+	$curl->setopt(CURLOPT_HEADER, 1);
+	$curl->setopt(CURLOPT_URL, 'https://broadcasthe.net/advent.php?action=claimprize');
+	$curl->setopt(CURLOPT_COOKIEFILE, $OPT_COOKIES);
+	$curl->setopt(CURLOPT_COOKIEJAR, $OPT_COOKIES);
+	$curl->setopt(CURLOPT_WRITEDATA, \$response_body);
+
+	my $retcode = $curl->perform();
+
+	if ($retcode == 0) {
+		if ($response_body =~ /<b>(\d+d \d+h \d+m \d+s)<\/b>/) {
+			verbose("Time left until next claim: $1\n");
+		} elsif ($response_body =~ /You have received the following prize:.*?<h1>(.*?)<\/h1>/) {
+			verbose("Yay, you got the following prize: $1\n");
 		} else {
-			verbose("An error happened: $retcode " . $curl->strerror($retcode) . " " . $curl->errbuf . "\n");
+			verbose("$response_body\n");
 		}
+	} else {
+		verbose("An error happened: $retcode " . $curl->strerror($retcode) . " " . $curl->errbuf . "\n");
+	}
+}
+
+sub main
+{
+	if ($OPT_LOGIN =~ /^(.+):(.+)$/) {
+		verbose("Logging in...\n");
+		btn_login($1, $2);
 	}
 
-	# Now we gotta sleep for a bit...
-	sleep($OPT_INTERVAL);
+	for (;;) {
+		btn_advent();
+
+		# Now we gotta sleep for a bit...
+		sleep($OPT_INTERVAL);
+	}
 }
